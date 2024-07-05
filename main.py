e9df786a6e8f26ef2369fce6a74171974fdadc47
NickTrinh <nhattvm1112@gmail.com>
2024-07-05 09:12:36 -0400

    model training script changes

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import torch
 from ultralytics import YOLO
 
 def run():
-    # Check if CUDA is available
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    model = YOLO("yolov8n.yaml")  # build a new model from scratch
+    model = YOLO("yolov8n.pt")  # build a new model from scratch
 
-    model.to(device) #Use GPU
-
-    model.train(data="config.yaml", epochs=600)  # train the model
+    model.train(data="data/data.yaml", epochs=100, device=0, batch=-1)  # train the model
 
 if __name__ == '__main__':
     run()
