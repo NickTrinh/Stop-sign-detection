e5d28dd52caef950eda131dc2c4c9bd3154c85d0
NickTrinh <nhattvm1112@gmail.com>
2024-07-05 09:13:06 -0400

    simplified to using ultralytics functions

--- predict_video.py
+++ predict_video.py
@@ -12,25 +12,17 @@ ret, frame = cap.read()
 H, W, _ = frame.shape
 out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'mp4v'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H)) # out video
 
-model_path = os.path.join('.', 'runs', 'detect', 'train16', 'weights', 'best.pt')
+model_path = os.path.join('.', 'runs', 'detect', 'train25', 'weights', 'best.pt')
 
 model = YOLO(model_path)  # load the model
 
-threshold = 0.5 # set threshold for conf score
-
 while ret:
-
-    results = model(frame)[0]
-
-    for result in results.boxes.data.tolist():
-        x1, y1, x2, y2, score, class_id = result
-
-        if score > threshold:
-            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
-            cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA) #apply bounding boxes
-
-    out.write(frame)
+    
+    results = model.predict(frame)
+    
+    plotted_frame = results[0].plot()
+    
+    out.write(plotted_frame)
     ret, frame = cap.read()
 
 cap.release()
